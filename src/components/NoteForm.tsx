a4c664b70c252ac44e3dba5037b9c21c9d3a521a
LoboReflexivo <darioguerragarcia.lr@gmail.com>
2024-04-25 17:25:44 +0200

    Basic useState ready

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,18 +1,47 @@
+"use client";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
 export default function NoteForm() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const router = useRouter();
+
   return (
-    <form>
+    <form
+      onSubmit={async (e) => {
+        try {
+          e.preventDefault();
+
+          console.log("title", title);
+          console.log("content", content);
+          const data = await axios.post("http://localhost:3000/api/notes", {
+            title: title,
+            content: content,
+          });
+          console.log("los datos son:", data.data);
+          router.refresh();
+        } catch (error) {
+          console.log(error);
+        }
+      }}
+    >
       <input
         type="text"
         name="title"
         autoFocus
         placeholder="Title"
         className="w-full px-4 py-2 text-black bg-white rounded-md focus-within:outline-none focus:ring-2 focus:ring-blue-600 my-2"
+        onChange={(e) => setTitle(e.target.value)}
       />
       <textarea
         name="content"
         autoFocus
         placeholder="Content"
-        className="w-full px-4 py-2 text-black bg-white rounded-md focus-within:outline-none focus:ring-2 focus:ring-blue-600 my-2"
+        className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2"
+        onChange={(e) => setContent(e.target.value)}
       />
       <button className="px-5 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700">
         Create
